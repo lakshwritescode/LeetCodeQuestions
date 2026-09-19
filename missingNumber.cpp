4eb6862e71bc932102a934768a38c914347119be
lakshwritescode <lakshaygoel1342004@gmail.com>
2026-09-19 16:59:43 +0530

    missing number

---
+++ missingNumber.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        int xor1 = 0;
+        int xor2 = 0;
+        for (int i = 0; i < n; i++)
+        {
+            xor1 = xor1 ^ nums[i];
+            xor2 = xor2 ^ (i + 1);
+        }
+        int ans = xor1 ^ xor2;
+        return ans;
+    }
+};
